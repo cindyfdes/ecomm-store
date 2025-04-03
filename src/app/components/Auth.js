aa0ffab133b172c3df527724c32dae9941aee5a0
Cindy Fernandes <fdes.cindy@gmail.com>
2025-04-04 00:43:02 +0530

    initial commit

---
+++ src/app/components/Auth.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export async function getServerSideProps() {
+  const res = await fetch("http://localhost:5000/api/hello");
+  const data = await res.json();
+
+  return {
+    props: { message: data.message },
+  };
+}
+const Auth = ({ message }) => {
+  return (
+    <div>
+      <h1>Welcome to My Next.js App!</h1>
+      <p>{message}</p>
+    </div>
+  );
+};
+
+export default Auth;
