4c248245e5ba116d7379c1a94858194dfe8e7283
Cindy Fernandes <fdes.cindy@gmail.com>
2025-04-13 21:29:30 +0530

    home componen fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,7 @@ import ProductsView from "./components/Products/products-view";
 import Layout from "./components/layout";
 import { CartProvider } from "./reducers/cart-reducer";
 import { Products } from "./models/Products";
-export default function Home({
-  products,
-  error,
-}: {
-  products: Products[];
-  error: string | null;
-}) {
+export default function Home() {
   return (
     <div className="  items-center justify-items-center min-h-screen  pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
